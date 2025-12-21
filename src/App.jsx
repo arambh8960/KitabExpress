d3ed9be45b93f3495bb94240a2d2c51e66ddc1f8
Arambh Tiwari <arambhtiwari@Arambhs-MacBook-Air.local>
2025-12-22 00:43:53 +0530

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from './Home/Home';
-
-import {Routes,Route} from "react-router-dom";
 import Courses from './courses/Courses';
-
 import Signup from './components/Signup';
 import Contact from './components/Contact';
 import About from './components/About';
 
-
-
 const App = () => {
   return (
-  
-   <>
-     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/course" element={<Courses/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/about" element={<About/>}/>
-      
-      </Routes> 
-     
-     
-</>
+    <BrowserRouter basename="/KitabExpress">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Courses />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
