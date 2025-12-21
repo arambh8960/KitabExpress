02b95ad344cb5ba1092b283113025bdd123f74e0
Arambh Tiwari <arambhtiwari@Arambhs-MacBook-Air.local>
2025-12-22 00:40:43 +0530

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ const App = () => {
   
    <>
      <Routes>
-      <Route path="./" element={<Home/>}/>
-      <Route path="./course" element={<Courses/>}/>
-      <Route path="./signup" element={<Signup/>}/>
-      <Route path="./contact" element={<Contact/>}/>
-      <Route path="./about" element={<About/>}/>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/course" element={<Courses/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/about" element={<About/>}/>
       
       </Routes> 
      
